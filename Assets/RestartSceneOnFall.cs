91420f85b16e447328fa0e1ce56d52f7a7f1217b
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-05-16 09:03:43 -0300

    rampas y cilindros

--- Assets/RestartSceneOnFall.cs
+++ Assets/RestartSceneOnFall.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class RestartSceneOnFall : MonoBehaviour
 {
+    public float deadFallHeight = -15;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class RestartSceneOnFall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < deadFallHeight)
+        {
+            Debug.Log("Respawn");
+            SceneManager.LoadScene("FPS Parkour");
+        }
     }
 }
