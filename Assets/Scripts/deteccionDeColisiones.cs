d0b36d6b0c889239e2799c50c4dd31bd6a1ffbc2
OliNaider <166539084+OliNaider@users.noreply.github.com>
2025-05-23 08:57:10 -0300

    colisiones

---
+++ Assets/Scripts/deteccionDeColisiones.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("contacto");
+    }
+}
